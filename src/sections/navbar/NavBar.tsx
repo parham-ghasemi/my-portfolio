328ef32cc16e66c74305f6266bf2d8f8d03c5820
parham-ghasemi <parham.ghasemi.1388@gmail.com>
2025-01-16 20:04:02 +0330

    Added backdrop-blur filter on navbar

--- src/sections/navbar/NavBar.tsx
+++ src/sections/navbar/NavBar.tsx
@@ -5,7 +5,7 @@ export default function NavBar() {
   const [isOpen, setIsOpen] = useState<boolean>(false)
   
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/80 backdrop-blur-[7px]">
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center py-5 mx-auto c-space">
           <a href="/" className="text-neutral-200 font-bold text-xl hover:text-white transition-colors">Parham</a>
